8912880486760428fe02baa50f89ff96e29111cb
Chase Whitfield <fatlard1993@gmail.com>
2019-12-24 18:32:43 -0800

    add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bamboo Block
+
+A [Minecraft](minecraft.net) mod built on [Fabric](fabricmc.net). This mod adds one block, a bamboo block.
---
+++ src/main/java/justfatlard/bamboo_block/BambooBlock.java
@@ -0,0 +1,22 @@
+package justfatlard.bamboo_block;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class BambooBlock implements ModInitializer {
+    public static final Block BAMBOO_BLOCK = new Block(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(0.5F, 2.5F).build());
+
+    @Override
+    public void onInitialize(){
+        Registry.register(Registry.BLOCK, new Identifier("bamboo-block-justfatlard", "bamboo_block"), BAMBOO_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("bamboo-block-justfatlard", "bamboo_block"), new BlockItem(BAMBOO_BLOCK, new Item.Settings().group(ItemGroup.DECORATIONS)));
+    }
+}
