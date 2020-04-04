03cc52037fd8ba33049425e1f6cd6d325f5db606
fatlard1993 <fatlard1993>
2020-04-03 18:58:03 -0700

    cleanup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Bamboo Block
 
-A [Minecraft](minecraft.net) mod built on [Fabric](fabricmc.net). This mod adds one block, a bamboo block.
+A [Minecraft](minecraft.net) mod built on [Fabric](fabricmc.net).
+
+This mod adds one block, a bamboo block. Use it to store your bamboo nice and compact. Maybe build a tall bamboo fence with it.
--- src/main/java/justfatlard/bamboo_block/BambooBlock.java
+++ src/main/java/justfatlard/bamboo_block/BambooBlock.java
@@ -12,11 +12,11 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BambooBlock implements ModInitializer {
-    public static final Block BAMBOO_BLOCK = new Block(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(0.6F, 16F).build());
+	public static final Block BAMBOO_BLOCK = new Block(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(0.6F, 16F).build());
 
-    @Override
-    public void onInitialize(){
-        Registry.register(Registry.BLOCK, new Identifier("bamboo-block-justfatlard", "bamboo_block"), BAMBOO_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("bamboo-block-justfatlard", "bamboo_block"), new BlockItem(BAMBOO_BLOCK, new Item.Settings().group(ItemGroup.DECORATIONS)));
-    }
+	@Override
+	public void onInitialize(){
+		Registry.register(Registry.BLOCK, new Identifier("bamboo-block-justfatlard", "bamboo_block"), BAMBOO_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier("bamboo-block-justfatlard", "bamboo_block"), new BlockItem(BAMBOO_BLOCK, new Item.Settings().group(ItemGroup.DECORATIONS)));
+	}
 }
